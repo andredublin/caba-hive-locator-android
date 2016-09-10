33cea40c9592e94fdcd380a6b8de209fa08268fe
Andre Dublin <81dublin@gmail.com>
2016-09-09 23:12:10 -0400

    add main activity

---
+++ app/src/main/java/com/penfolddev/cabahivelocator/MainActivity.java
@@ -0,0 +1,54 @@
+package com.penfolddev.cabahivelocator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+
+    @NeedsPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void accessLocation() {
+        // navigate to maps activity
+    }
+
+    @OnShowRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void showLocationRationale(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_location_rationale)
+                .setPositiveButton(getString(R.string.button_allow), (dialog, button) -> request.proceed())
+                .setNegativeButton(getString(R.string.button_deny), (dialog, button) -> request.cancel())
+                .show();
+    }
+
+    @OnPermissionDenied(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void showDeniedForLocation() {
+        Toast.makeText(this, R.string.permission_location_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(android.Manifest.permission.ACCESS_FINE_LOCATION)
+    void showNeverAskForLocation() {
+        Toast.makeText(this, R.string.permission_location_denied, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/penfolddev/cabahivelocator/MapsActivity.java
+++ app/src/main/java/com/penfolddev/cabahivelocator/MapsActivity.java
@@ -1,19 +1,33 @@
 package com.penfolddev.cabahivelocator;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleMap.OnCameraMoveListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MapsActivity";
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +39,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnMapLongClickListener(longClickListener());
         mMap.setOnCameraMoveListener(this);
+        buildGoogleApiClient();
+        mMap.setMyLocationEnabled(true);
+    }
+
+    private void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     private GoogleMap.OnMapLongClickListener longClickListener() {
@@ -56,5 +72,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onCameraMove() {
         // clear pin if pin exists
         // hide form if form showing
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LatLng target = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+        CameraPosition.Builder builder = new CameraPosition.Builder();
+        builder.zoom(mMap.getMaxZoomLevel() / 1.5f);
+        builder.target(target);
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(builder.build()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+        Toast.makeText(this, connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
     }
 }
